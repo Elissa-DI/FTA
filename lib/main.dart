f49b27af660b7a564ba48a634013a7b8ac4e781c
Elissa-DI <elissadusabe@gmail.com>
2024-05-08 09:08:13 +0200

    added tiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:first01/pages/home_page.dart';
+// ignore_for_file: prefer_const_constructors, use_super_parameters
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: HomePage(),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:first01/pages/home_page.dart';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:first01/utils/todo_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,6 +9,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // List todoList = [
+  //   {"make tutorial", false},
+  //   {"Do exercise", false},
+  // ];
+
+  List<Map<String, dynamic>> todoList = [
+    {"taskName": "make tutorial", "taskCompleted": false},
+    {"taskName": "Do exercise", "taskCompleted": false},
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      // todoList[index][0] = !todoList[index][1];
+      todoList[index]["taskCompleted"] = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +33,19 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('TO DO'),
         elevation: 0,
+        backgroundColor: Colors.blue,
       ),
-      body: ListView(
-        children: [
-          
-        ],
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            // taskName: todoList[index][0],
+            // taskCompleted: todoList[index][1],
+            taskName: todoList[index]["taskName"],
+            taskCompleted: todoList[index]["taskCompleted"],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
